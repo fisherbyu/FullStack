3c97aa3a01b98486ce081bf0d0f475f7d050eab1
fisherbyu <fisherandrew777@gmail.com>
2023-04-04 16:52:53 -0600

    Update React Formating

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,30 @@
 import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
-import Header from './Header';
+import NavBar from './Banner';
+import Podcasts from './Podcasts';
+import Home from './Home';
+// import Header from './Header';
 import MovieList from './movie/MovieList';
 
 function App() {
+  //Define Path for Page Display
+  const path = window.location.pathname;
+  let content;
+  // Allow for changing of the Routes
+  content = <Home />
+  if (path === '/movies') {
+      content = <MovieList />;
+  }
+  else if (path === '/podcasts') {
+      content = <Podcasts />;
+  }
+  //Render out Selected Content with Common Navbar
   return (
-    <div className="App">
-      <Header/>
-      <MovieList/>
-    </div>
+      <div className="App">
+      <NavBar />
+      {content}
+      </div>
   );
-}
+  }
 
 export default App;
---
+++ frontend/src/Home.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import joel from './1.jpg'
+
+function Home(props: any) {
+    //Basic Home Page
+    return (
+        <React.Fragment>
+            <br/>
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <h1>Welcome to My Site!</h1>
+                        <img src={joel} alt="Joel Hilton" style={{"width": "300px"}}/>
+                    </div>
+                </div>
+            </div>
+        </React.Fragment>
+    );
+}
+
+export default Home;
